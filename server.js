5a4e2cf4e0001dfac184ed0402a52a105fc63624
Roman <roma.buchuk@gmail.com>
2015-04-24 10:45:20 +0300

    lec 10

--- server.js
+++ server.js
@@ -2,3 +2,26 @@
  * Created by Roman on 20.04.2015.
  */
 var express = require('express');
+var app  = express();
+var bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
+
+app.get('/info', function(req, res, next){
+    res.status(200).send({pupkin: 'Hello World'});
+});
+
+app.post('/info', function(req, res, next){
+    var body = req.body;
+
+    if(!body.key){
+        var err = new Error('Not Key');
+
+        return res.status(500).send({error: err.message + ' ' + err.stack});
+    }
+    res.status(200).send(body);
+});
+
+app.listen(3030, function(){
+    console.log('Server start on port = 3030');
+});
