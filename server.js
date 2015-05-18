1c0e3815b92a71783b3599dc344caefb5cc6e40d
Roman Buchuk <roma.buchuk@gmail.com>
2015-05-18 16:54:28 +0100

    async

--- server.js
+++ server.js
@@ -7,61 +7,80 @@ var userUrl = 'mongodb://localhost:27017/userDb';
 var postUrl = 'mongodb://localhost:27017/postDb';
 var async = require('async');
 
-var userBb = mongoose.createConnection(userUrl, function () {
-    var postBb = mongoose.createConnection(postUrl, function () {
-        app.use(logger('dev'));
-        app.use(bodyParser.json());
-
-        require('./models/index.js');
-
-        app.get('/', function (req, res, next) {
-
-            console.log('----------------------------');
-
-            res.status(200).send('qwerty');
-        });
-
-        app.post('/user', function (req, res, next) {
-            var body = req.body;
-
-            var User = mongoose.model('user');
-            var user = new User(body);
-
-            user.save(function (err, user) {
-                if (err) {
-                    return res.status(500).send(err)
-                }
-
-                res.status(200).send(user);
-            });
-
-
-        });
-
-        app.get('/user', function (req, res, next) {
-
-            var User = mongoose.model('user');
-
-            User.find(function (err, users) {
-                if (err) {
-                    return res.status(500).send(err)
-                }
-
-                res.status(200).send(users);
-            });
-        });
-
-        app.listen(3030, function () {
-            console.log('Server start on port = 3030');
-        });
+function userConnect(callback){
+    var userDb = mongoose.createConnection(userUrl, function(){
+        callback(null, userDb);
     });
-    postBb.on('error', function (err) {
+    userDb.on('error', function(err){
+        callback(err);
+    })
+};
+
+function postConnect(callback){
+    var postDb = mongoose.createConnection(postUrl, function(){
+        callback(null, postDb);
+    });
+    postDb.on('error', function(err){
+        callback(err);
+    })
+};
+
+var asyncObject = {
+    userDb: userConnect,
+    postDb: postConnect
+};
+
+async.parallel(asyncObject, function(err, result){
+    if(err){
         console.error(err);
-    });
-});
+        process.exit(1);
+    }
 
-userBb.on('error', function (err) {
-    console.error(err);
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+
+    require('./models/index.js');
+
+    app.get('/', function (req, res, next) {
+
+        console.log('----------------------------');
+
+        res.status(200).send('qwerty');
+    });
+
+    app.post('/user', function (req, res, next) {
+        var body = req.body;
+
+        var User = mongoose.model('user');
+        var user = new User(body);
+
+        user.save(function (err, user) {
+            if (err) {
+                return res.status(500).send(err)
+            }
+
+            res.status(200).send(user);
+        });
+
+
+    });
+
+    app.get('/user', function (req, res, next) {
+
+        var User = mongoose.model('user');
+
+        User.find(function (err, users) {
+            if (err) {
+                return res.status(500).send(err)
+            }
+
+            res.status(200).send(users);
+        });
+    });
+
+    app.listen(3030, function () {
+        console.log('Server start on port = 3030');
+    });
 });
 
 process.env.NODE_ENV = 'production';
