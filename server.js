7e2027b059a02dd70c74c71cc0a594a39b949cba
Roman Buchuk <roma.buchuk@gmail.com>
2015-05-18 16:26:34 +0100

    multiConnect1

--- server.js
+++ server.js
@@ -3,61 +3,64 @@ var app = express();
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var url = 'mongodb://localhost:28017/jsGroup2';
+var userUrl = 'mongodb://localhost:27017/userDb';
+var postUrl = 'mongodb://localhost:27017/postDb';
+var async = require('async');
 
-mongoose.connect(url);
+var userBb = mongoose.createConnection(userUrl, function () {
+    var postBb = mongoose.createConnection(postUrl, function () {
+        app.use(logger('dev'));
+        app.use(bodyParser.json());
 
-var db = mongoose.connection;
+        require('./models/index.js');
 
-db.once('open', function () {
-    app.use(logger('dev'));
-    app.use(bodyParser.json());
+        app.get('/', function (req, res, next) {
 
-    require('./models/index.js');
+            console.log('----------------------------');
 
-    app.get('/', function (req, res, next) {
-
-        console.log('----------------------------');
-
-        res.status(200).send('qwerty');
-    });
-
-    app.post('/user', function (req, res, next) {
-        var body = req.body;
-
-        var User = mongoose.model('user');
-        var user = new User(body);
-
-        user.save(function (err, user) {
-            if (err) {
-                return res.status(500).send(err)
-            }
-
-            res.status(200).send(user);
+            res.status(200).send('qwerty');
         });
 
+        app.post('/user', function (req, res, next) {
+            var body = req.body;
 
-    });
+            var User = mongoose.model('user');
+            var user = new User(body);
 
-    app.get('/user', function (req, res, next) {
+            user.save(function (err, user) {
+                if (err) {
+                    return res.status(500).send(err)
+                }
 
-        var User = mongoose.model('user');
+                res.status(200).send(user);
+            });
 
-        User.find(function (err, users) {
-            if (err) {
-                return res.status(500).send(err)
-            }
 
-            res.status(200).send(users);
+        });
+
+        app.get('/user', function (req, res, next) {
+
+            var User = mongoose.model('user');
+
+            User.find(function (err, users) {
+                if (err) {
+                    return res.status(500).send(err)
+                }
+
+                res.status(200).send(users);
+            });
+        });
+
+        app.listen(3030, function () {
+            console.log('Server start on port = 3030');
         });
     });
-
-    app.listen(3030, function () {
-        console.log('Server start on port = 3030');
+    postBb.on('error', function (err) {
+        console.error(err);
     });
 });
 
-db.on('error', function (err) {
+userBb.on('error', function (err) {
     console.error(err);
 });
 
