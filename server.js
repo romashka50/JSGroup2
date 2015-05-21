34b4939b19d0b29bbe0de5f0ee8866b4fd8e193a
Roman <roma.buchuk@gmail.com>
2015-05-21 18:59:05 +0300

    pg

--- server.js
+++ server.js
@@ -1,64 +1,70 @@
 var express = require('express');
 var app = express();
 var logger = require('morgan');
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var url = 'mongodb://localhost:28017/jsGroup2';
+var Bookshelf = require('bookshelf');
+var Knex = require ('knex');
+var knex;
+var PostGre;
 
-mongoose.connect(url);
+app.use(logger('dev'));
+app.use(bodyParser.json());
 
-var db = mongoose.connection;
+knex = Knex({
+    debug: true,
+    client: 'pg',
+    connection: {
+        host: '192.168.88.250',
+        user: 'postgres',
+        password: 'postgres',
+        port: '5432',
+        database: 'jsGroup'
+    }
+});
 
-db.once('open', function () {
-    app.use(logger('dev'));
-    app.use(bodyParser.json());
+PostGre = Bookshelf(knex);
 
-    require('./models/index.js');
 
-    app.get('/', function (req, res, next) {
+app.get('/', function (req, res, next) {
 
-        console.log('----------------------------');
+    console.log('----------------------------');
 
-        res.status(200).send('qwerty');
+    res.status(200).send('qwerty');
+});
+
+app.post('/user', function (req, res, next) {
+    var body = req.body;
+
+    var User = mongoose.model('user');
+    var user = new User(body);
+
+    user.save(function (err, user) {
+        if (err) {
+            return res.status(500).send(err)
+        }
+
+        res.status(200).send(user);
     });
 
-    app.post('/user', function (req, res, next) {
-        var body = req.body;
 
-        var User = mongoose.model('user');
-        var user = new User(body);
+});
 
-        user.save(function (err, user) {
-            if (err) {
-                return res.status(500).send(err)
-            }
+app.get('/user', function (req, res, next) {
 
-            res.status(200).send(user);
-        });
+    var User = mongoose.model('user');
 
+    User.find(function (err, users) {
+        if (err) {
+            return res.status(500).send(err)
+        }
 
-    });
-
-    app.get('/user', function (req, res, next) {
-
-        var User = mongoose.model('user');
-
-        User.find(function (err, users) {
-            if (err) {
-                return res.status(500).send(err)
-            }
-
-            res.status(200).send(users);
-        });
-    });
-
-    app.listen(3030, function () {
-        console.log('Server start on port = 3030');
+        res.status(200).send(users);
     });
 });
 
-db.on('error', function (err) {
-    console.error(err);
+app.listen(3030, function () {
+    console.log('Server start on port = 3030');
 });
 
 process.env.NODE_ENV = 'production';
